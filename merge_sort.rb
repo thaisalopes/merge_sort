d74c650ad8f847877d1de23c8b8840c7bf432188
ThaisaLopes <ta_lemos@hotmailcom>
2022-07-04 22:59:24 -0300

    Merge Sort

---
+++ merge_sort.rb
@@ -0,0 +1,15 @@
+def merge_sort(array)
+  array1 = array.slice(0,(array.length/2)).to_a
+  array2 = array.slice((array.length/2),array.length).to_a
+  print array1
+  print array2
+end
+
+#def merge_sort_part2(array1,array2)
+#  if array1[0] < array2[0]
+
+    
+
+array1 = [1,6]
+array2 = [2]
+merge_sort([3,2,5,6,1])
